c632c293032ecbbfcc213f876d046c415b4e3017
dylan <dylan@DESKTOP-DCC9ED2>
2022-01-12 11:30:44 +0000

    Add project files.

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "Helloworld";
+        }
+        public string welcome()
+        {
+            return "Helloworld welcome!!";
+        }
+    }
+}
